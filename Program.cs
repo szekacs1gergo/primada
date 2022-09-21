cd155f3c582962990289f190ff0fea891356d4b2
szekacs1gergo <43171768+szekacs1gergo@users.noreply.github.com>
2022-09-21 12:28:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primszamkeres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam;
+            Console.WriteLine("Kérek egy számot: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            bool nemprim = false;
+            try
+            {
+                for (int i = 0; i <= szam; i++)
+                {
+                    if (n%szam==0)
+                    {
+                        nemprim = false;
+                        Console.WriteLine("A(z) {0} prím szám",n);
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+
+                throw;
+            }
+        }
+    }
+}
